5c240d125aa96ba2dbb07f0e86de34406b0c4eef
wangxingfei <xingfeiwang6@gmail.com>
2025-08-01 21:20:16 +0800

    uodata

--- router/personal/inspiration.js
+++ router/personal/inspiration.js
@@ -1,32 +1,8 @@
 export function personalInspiration() {
   return [
     {
-      collapsible: true,
-      text: 'App设计思路',
-      items: [
-        { text: '概述', link: '/personal/app-design/' },
-      ],
-    },
-    {
-      collapsible: true,
-      text: '业务发现与创新',
-      items: [
-        { text: '概述', link: '/personal/business-innovation/' },
-      ],
-    },
-    {
-      collapsible: true,
-      text: '产品思维',
-      items: [
-        { text: '概述', link: '/personal/product-thinking/' },
-      ],
-    },
-    {
-      collapsible: true,
-      text: '用户体验设计',
-      items: [
-        { text: '概述', link: '/personal/ux-design/' },
-      ],
+      text: '创作灵感',
+      link: '/personal/',
     },
   ]
 } 
